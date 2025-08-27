09c96058b0430294f778775629e4e3bb68bcc6de
afsaanchez04 <afsanchez04@gmail.com>
2025-08-26 21:48:00 -0500

    ruta protegida en app.jsx

--- 3-full-stack/frontend/src/App.jsx
+++ 3-full-stack/frontend/src/App.jsx
@@ -3,18 +3,27 @@ import { HomePage } from "./pages/HomePage"
 import { RegisterPage } from "./pages/RegisterPage"
 import { LoginPage } from "./pages/LoginPage"
 import { ProfilePage } from "./pages/ProfilePage"
+import { PrivateRoute } from "./PrivateRoute"
 
 
 export const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage/>} />
-        <Route path="/register" element={<RegisterPage/>} />
-        <Route path="/login" element={<LoginPage/>} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
 
+        {/* Ruta privada */}
 
-        <Route path="/profile" element={<ProfilePage/>} />
+        <Route
+          path="/profile"
+          element={
+            <PrivateRoute>
+              <ProfilePage />
+            </PrivateRoute>
+          }
+        />
 
       </Routes>
     </BrowserRouter>
