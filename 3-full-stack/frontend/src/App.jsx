20df67ac5f532ceae5543c9a35b1559b2a149748
afsaanchez04 <afsanchez04@gmail.com>
2025-08-26 20:45:55 -0500

    rutas establecidas

--- 3-full-stack/frontend/src/App.jsx
+++ 3-full-stack/frontend/src/App.jsx
@@ -1,12 +1,19 @@
-import FormularioAlumno from "./components/FormularioAlumno"
-import ListaAlumnos from "./components/ListaAlumnos"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { HomePage } from "./pages/HomePage"
+import { RegisterPage } from "./pages/RegisterPage"
+import { LoginPage } from "./pages/LoginPage"
+import { ProfilePage } from "./pages/ProfilePage"
 
 
 export const App = () => {
   return (
-    <>
-      <FormularioAlumno/>
-      <ListaAlumnos/>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage/>} />
+        <Route path="/register" element={<RegisterPage/>} />
+        <Route path="/login" element={<LoginPage/>} />
+        <Route path="/profile" element={<ProfilePage/>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
