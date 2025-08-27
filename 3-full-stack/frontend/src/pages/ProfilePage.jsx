bfdf352d75a722a465a27a4bb8e7a0996aae4423
afsaanchez04 <afsanchez04@gmail.com>
2025-08-26 21:44:44 -0500

    ProfilePage terminada

--- 3-full-stack/frontend/src/pages/ProfilePage.jsx
+++ 3-full-stack/frontend/src/pages/ProfilePage.jsx
@@ -1,7 +1,42 @@
-
+import { useEffect, useState } from "react"
+import { logoutRequest, profileRequest } from "../api/auth"
+import { useNavigate } from "react-router-dom"
 
 export const ProfilePage = () => {
+
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    const checkProfile = async () => {
+      try {
+        const res = await profileRequest()
+        setUser(res.data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    checkProfile()
+  }, [])
+
+  const navigate = useNavigate()
+
+  const handleLogout = async () => {
+    await logoutRequest()
+    navigate("/login")
+  }
+
   return (
-    <div>ProfilePage 🤖</div>
+    <div>
+      <div>Perfil</div>
+      {user ? (
+        <div>
+          <p>Nombre: {user.username}</p>
+          <p>Email: {user.email}</p>
+          <button onClick={handleLogout}>Cerrar sesión</button>
+        </div>
+      ) : (
+        <p>Cargando perfil...</p>
+      )}
+    </div>
   )
 }
