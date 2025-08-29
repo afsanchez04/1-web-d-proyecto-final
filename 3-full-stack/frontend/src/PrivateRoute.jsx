efd3168c7798244cd75faabda207a6c6a818688c
afsaanchez04 <afsanchez04@gmail.com>
2025-08-28 21:27:48 -0500

    PrivateRoute + AuthContext

--- 3-full-stack/frontend/src/PrivateRoute.jsx
+++ 3-full-stack/frontend/src/PrivateRoute.jsx
@@ -1,24 +1,13 @@
-import { useEffect, useState } from "react"
-import { api } from "./services/api"
+
 import { Navigate } from "react-router-dom"
+import { useAuth } from "./context/AuthContext"
 
 export const PrivateRoute = ({children}) => {
 
-  const [auth, setAuth] = useState(null)
+  const { user, loading } = useAuth()
 
-  useEffect(() => {
-    const checkAuth = async () => {
-      try {
-        await api.get("/profile")
-        setAuth(true)
-      } catch (error) {
-        console.log(error)
-        setAuth(false)
-      }
-    }
-    checkAuth()
-  }, [])
+  if(loading) return <p>Cargando...</p>
   
 
-  return auth ? children : <Navigate to="/login"/>
+  return user ? children : <Navigate to="/login"/>
 }
