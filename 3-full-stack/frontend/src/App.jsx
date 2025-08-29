0ad8bc17defc9da539eefab4db2d567027f1193a
afsaanchez04 <afsanchez04@gmail.com>
2025-08-28 21:22:19 -0500

    login + AuthContext

--- 3-full-stack/frontend/src/App.jsx
+++ 3-full-stack/frontend/src/App.jsx
@@ -4,28 +4,31 @@ import { RegisterPage } from "./pages/RegisterPage"
 import { LoginPage } from "./pages/LoginPage"
 import { ProfilePage } from "./pages/ProfilePage"
 import { PrivateRoute } from "./PrivateRoute"
+import { AuthProvider } from "./context/AuthContext"
 
 
 export const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/login" element={<LoginPage />} />
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/login" element={<LoginPage />} />
 
-        {/* Ruta privada */}
+          {/* Ruta privada */}
 
-        <Route
-          path="/profile"
-          element={
-            <PrivateRoute>
-              <ProfilePage />
-            </PrivateRoute>
-          }
-        />
+          <Route
+            path="/profile"
+            element={
+              <PrivateRoute>
+                <ProfilePage />
+              </PrivateRoute>
+            }
+          />
 
-      </Routes>
-    </BrowserRouter>
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   )
 }
--- 3-full-stack/frontend/src/pages/LoginPage.jsx
+++ 3-full-stack/frontend/src/pages/LoginPage.jsx
@@ -1,8 +1,11 @@
 import { useForm } from "react-hook-form";
 import { Link, useNavigate } from "react-router-dom";
 import { loginRequest } from "../api/auth.js";
+import { useAuth } from "../context/AuthContext.jsx";
 
 export const LoginPage = () => {
+
+  const { setUser } = useAuth()//Llamar al custom hook useAuth para recuperar la función de actualizar usuario
 
   const {
     register,
@@ -18,7 +21,7 @@ export const LoginPage = () => {
 
     try {
       const res = await loginRequest({ email, password })
-      console.log(res)
+      setUser(res.data)
       navigate("/profile")
     } catch (error) {
       console.log(error) 
