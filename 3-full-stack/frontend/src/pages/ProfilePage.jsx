0367e9e4826ae8cfc8b3fdeee29cb90c3a665226
afsaanchez04 <afsanchez04@gmail.com>
2025-08-28 21:24:31 -0500

    profile + AuthContext

--- 3-full-stack/frontend/src/pages/ProfilePage.jsx
+++ 3-full-stack/frontend/src/pages/ProfilePage.jsx
@@ -1,29 +1,8 @@
-import { useEffect, useState } from "react"
-import { logoutRequest, profileRequest } from "../api/auth"
-import { useNavigate } from "react-router-dom"
+import { useAuth } from "../context/AuthContext"
 
 export const ProfilePage = () => {
 
-  const [user, setUser] = useState(null)
-
-  useEffect(() => {
-    const checkProfile = async () => {
-      try {
-        const res = await profileRequest()
-        setUser(res.data)
-      } catch (error) {
-        console.log(error)
-      }
-    }
-    checkProfile()
-  }, [])
-
-  const navigate = useNavigate()
-
-  const handleLogout = async () => {
-    await logoutRequest()
-    navigate("/login")
-  }
+  const { user, logout } = useAuth()
 
   return (
     <div>
@@ -32,7 +11,7 @@ export const ProfilePage = () => {
         <div>
           <p>Nombre: {user.username}</p>
           <p>Email: {user.email}</p>
-          <button onClick={handleLogout}>Cerrar sesión</button>
+          <button onClick={logout}>Cerrar sesión</button>
         </div>
       ) : (
         <p>Cargando perfil...</p>
